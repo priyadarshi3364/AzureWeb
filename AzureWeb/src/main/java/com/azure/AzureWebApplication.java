9686979cd32d0028fdf771a6a4cdcaa476548c06
kumar <kumar@Priyadarshi>
2018-06-25 23:29:02 +0530

    initial

---
+++ AzureWeb/src/main/java/com/azure/AzureWebApplication.java
@@ -0,0 +1,12 @@
+package com.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureWebApplication.class, args);
+	}
+}
